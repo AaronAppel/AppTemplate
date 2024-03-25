2610d6fccdea53b182fe25364dd64833f719046e
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2024-03-24 21:50:21 -0400

    [Mirror] Changes for Mirror library
    Committing in place of QwerkE dev branch commit history

---
+++ Libraries/Mirror/Build-Mirror.lua
@@ -0,0 +1,17 @@
+project "Mirror"
+	kind "StaticLib"
+	location ""
+	
+	files
+	{ 
+		"Source/Mirror.h",
+		"Source/MirrorTypes.h",
+		
+		"Source/MirrorEnumPrimitives.txt",
+		
+		"Source/Mirror.cpp",
+	}
+
+	includedirs	{ }
+
+	defines { }
